235e340e5a7e1614ac3ee2b4d7ad7bc82d9883de
Luis310 <lrso.10@hotmail.com>
2016-08-05 01:48:30 -0600

    empezando a separar palabras

--- main.c
+++ main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+void llamada(){
+printf("Ingrese el comando indicado \n");
+scanf("%s");
+
+}
+
+
+
+
 int main()
 {
+
+    llamada();
 
     return 0;
 }
