ad2697b7abf5ea1ffe1ff75ecb38fa288eb9ce9c
Luis310 <lrso.10@hotmail.com>
2016-08-08 13:04:12 -0600

    Compuse algo de los Struct

--- main.c
+++ main.c
@@ -5,14 +5,6 @@
 
 time_t t;
 
-typedef struct archivo{
-
-int mbr_tam;
-char tiempo[100];
-int numDisco;
-
-}MBR;
-
 typedef struct p{
 
 char estado[1];
@@ -23,8 +15,8 @@ int tam;
 char nombre[16];
 }particion;
 
-typedef struct e{
 
+typedef struct e{
 char estado[1];
 char fit[2];
 int inicio;
@@ -33,21 +25,35 @@ int proximoEbr;
 char nombre[16];
 }ebr;
 
-
+typedef struct archivo{
+int mbr_tam;
+char tiempo[100];
+int numDisco;
+particion p1;
+particion p2;
+particion p3;
+particion p4;
+}MBR;
 
 
 void llamada(){
+char var;
 printf("Ingrese el comando indicado \n");
 char cadena[200];
 fgets(cadena,200,stdin);
-printf("%s",cadena);
+//printf("%s",cadena);
 
 char * pch;
   pch = strtok (cadena," ,.-");
   while (pch != NULL)
   {
-    printf ("%s\n",pch);
+
+  if(strcmp(pch,"mkdisk")){
+    break;
+  }else{
+   printf ("%s\n",pch);
     pch = strtok (NULL, " ,.-");
+  }
   }
 }
 
